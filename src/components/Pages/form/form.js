feeacdd974309c18e58efe93de38ae94fc1ee41c
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-04 00:16:17 +0300

    create skeleton for userform section

---
+++ src/components/Pages/form/form.js
@@ -0,0 +1,11 @@
+import Aside from "../User/aside";
+
+export default function Form() {
+  return (
+    <section id="userForm">
+      <Aside />
+      <form></form>
+      <div></div>
+    </section>
+  );
+}
--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,13 @@
 import Landing from "./Pages/landing/landing";
 import Login from "./Pages/Login/login";
+import Form from "./Pages/form/form";
 
 export default function Main() {
   return (
     <>
       <Landing />
       <Login />
+      <Form />
     </>
   );
 }
