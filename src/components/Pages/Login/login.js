4afb326ed32bedc5d6cec8ecb2c8d7b4b0e7c543
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-04 01:08:23 +0300

    create login form

--- src/components/Pages/Login/login.js
+++ src/components/Pages/Login/login.js
@@ -1,8 +1,23 @@
 export default function Login() {
   return (
     <div id="forms">
-      <form id="signup"></form>
-      <form id="login"></form>
+      <form id="signup">
+        <div>
+          <input type="text" placeholder="Your firstname" />
+          <input type="text" placeholder="Your lastname" />
+          <input type="email" placeholder="Your Email" />
+          <input type="password" placeholder="Enter password" />
+          <input type="password" placeholder="Confirm password" />
+          <button>Join us</button>
+        </div>
+      </form>
+      <form id="login">
+        <div>
+          <input type="email" placeholder="Your Email" />
+          <input type="password" placeholder="Your Password" />
+          <button>Continue</button>
+        </div>
+      </form>
     </div>
   );
 }
