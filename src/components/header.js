1e4239f76ded39026d6b66517eb856d75a47603a
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-03 23:14:34 +0300

    create login and user pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,11 @@
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <span></span>
+      <span></span>
+      <span>KUPID</span>
+      <span>Features</span>
+      <span>Log In</span>
+    </header>
+  );
 }
