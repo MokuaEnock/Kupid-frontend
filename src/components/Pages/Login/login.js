d7bf23087df33c2218b705bae1650a299e527269
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-03 23:29:41 +0300

    create skeleton for forms

---
+++ src/components/Pages/Login/login.js
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div id="forms">
+      <form id="signup"></form>
+      <form id="login"></form>
+    </div>
+  );
+}
--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,11 @@
 import Landing from "./Pages/landing/landing";
+import Login from "./Pages/Login/login";
 
 export default function Main() {
   return (
     <>
       <Landing />
+      <Login />
     </>
   );
 }
