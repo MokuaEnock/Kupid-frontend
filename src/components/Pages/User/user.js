f20986ff463e844474636406cd1fb168f50dae54
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-04 03:36:22 +0300

    create skeleton for user page

--- src/components/Pages/User/user.js
+++ src/components/Pages/User/user.js
@@ -4,7 +4,15 @@ export default function User() {
   return (
     <section id="user">
       <Aside />
-      <div id="users"></div>
+      <div id="users">
+        <div id="user_title">Enock Mokua</div>
+        <div id="user_image"></div>
+        <div id="user_info"></div>
+        <div id="user_buttons">
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
+      </div>
       <div id="bio">
         <form>
           <input type="text" placeholder="Search..." />
