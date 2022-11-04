71bacc6f374053b6eab65657be90681cad0ff9de
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-04 04:32:06 +0300

    format aside

--- src/components/Pages/User/aside.js
+++ src/components/Pages/User/aside.js
@@ -2,10 +2,9 @@ export default function Aside() {
   return (
     <aside>
       <span>Home</span>
-      <span>Home</span>
-      <span>Home</span>
-      <span>Home</span>
-      <span>Home</span>
+      <span>Message</span>
+      <span>Profile</span>
+      <span>Exit</span>
     </aside>
   );
 }
