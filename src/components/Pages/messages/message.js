3c7056713d903efcc278093eb9de7e34354ed86f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-07 17:27:20 +0300

    restyle message list

--- src/components/Pages/messages/message.js
+++ src/components/Pages/messages/message.js
@@ -1,11 +1,27 @@
 import Aside from "../User/aside.js";
 
 export default function Message() {
+  function changeColor() {
+    let disp = document.querySelector("#message_display");
+    if (disp.style.background === "white") {
+      disp.style.background = "red";
+    } else {
+      disp.style.background = "white";
+    }
+  }
+
   return (
     <section id="message">
       <Aside />
       <section>
-        <div id="message_list"></div>
+        <div id="message_list">
+          <li onClick={changeColor}>helllo wwjjwjwjwjjw</li>
+          <li>Message 1</li>
+          <li>Message 1</li>
+          <li>Message 1</li>
+          <li>Message 1</li>
+          <li>Message 1</li>
+        </div>
 
         <div id="message_display"></div>
       </section>
