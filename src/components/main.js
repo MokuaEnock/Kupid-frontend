4f94abe850146b0057c3b264e740068eaad82fa2
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-07 18:27:32 +0300

    fetch data

--- src/components/main.js
+++ src/components/main.js
@@ -1,3 +1,4 @@
+import react, { useEffect, useState } from "react";
 import Landing from "./Pages/landing/landing";
 import Login from "./Pages/Login/login";
 import Form from "./Pages/form/form";
@@ -5,6 +6,15 @@ import User from "./Pages/User/user";
 import Message from "./Pages/messages/message.js";
 
 export default function Main() {
+  let [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/users")
+      .then((r) => r.json())
+      .then((r) => setUsers(r));
+  }, []);
+
+  console.log(users);
   return (
     <>
       <Landing />
